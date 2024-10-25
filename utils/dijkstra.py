b472be380add941c1edc20b76c38ee6bcd436122
Juan Cuevas <19-10056@usb.ve>
2024-10-24 22:11:59 -0400

    Agregando implementación de algoritmos A* y Dijkstra, y definiendo clases para grafo y nodos

---
+++ utils/dijkstra.py
@@ -0,0 +1,36 @@
+from typing import List
+from utils.graph import Graph, Node, reconstruct_path
+
+def dijkstra(graph: Graph, start: Node, end: Node) -> List[Node]:
+    open_set: list = []
+    closed_set: list = []
+    came_from: dict = {}
+    g_score: dict = {}
+    f_score: dict = {}
+
+    for node in graph.get_nodes():
+        g_score[node] = float('inf')
+        f_score[node] = float('inf')
+    g_score[start] = 0
+    f_score[start] = start.coords.distance_to(end.coords)
+    open_set.append(start)
+
+    while open_set:
+        current: Node = min(open_set, key=lambda node: f_score[node])
+        if current == end:
+            return reconstruct_path(came_from, current)
+        open_set.remove(current)
+        closed_set.append(current)
+        for connection in graph.get_connections(current):
+            neighbor: Node = connection.get_other_node(current)
+            if neighbor in closed_set:
+                continue
+            tentative_g_score: float = g_score[current] + connection.get_cost()
+            if neighbor not in open_set:
+                open_set.append(neighbor)
+            elif tentative_g_score >= g_score[neighbor]:
+                continue
+            came_from[neighbor] = current
+            g_score[neighbor] = tentative_g_score
+            f_score[neighbor] = g_score[neighbor] + neighbor.coords.distance_to(end.coords)
+    return []
